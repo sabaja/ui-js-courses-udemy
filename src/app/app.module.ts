496feae7a945febff138ed5b6715ae3e9a28fa53
sabaja <jacoposabatini76@gmail.com>
2022-08-31 07:29:20 +0200

    Add rating stars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,26 @@ import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { BackButtonDirective } from './directive/back-button.directive';
+import { RatingComponent } from './rating/rating.component';
+import { NgxStarRatingModule } from 'ngx-star-rating'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseComponent,
     HomepageComponent,
-    BackButtonDirective
+    BackButtonDirective,
+    RatingComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    NgxStarRatingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
